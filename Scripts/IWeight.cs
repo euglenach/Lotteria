0e3991d9ba7173a0bd880e162958d6533f36a3cd
euglenach <midorimushiiii@gmail.com>
2024-05-17 19:11:18 +0900

    IWeightのプロキシと構造体版のやつ

--- Scripts/IWeight.cs
+++ Scripts/IWeight.cs
@@ -14,4 +14,38 @@
             this.Weight = weight;
         }
     }
+    
+    public readonly struct DefaultWeightStruct : IWeight
+    {
+        public int Weight{get;}
+
+        public DefaultWeightStruct(int weight)
+        {
+            this.Weight = weight;
+        }
+    }
+
+    public class WeightProxy<T> : IWeight
+    {
+        public readonly T Target;
+        public int Weight{ get; }
+
+        public WeightProxy(T target, int weight)
+        {
+            Target = target;
+            Weight = weight;
+        }
+    }
+    
+    public readonly struct WeightProxyStruct<T> : IWeight
+    {
+        public readonly T Target;
+        public int Weight{ get; }
+
+        public WeightProxyStruct(T target, int weight)
+        {
+            Target = target;
+            Weight = weight;
+        }
+    }
 }
